ac6132e3070dc31e9c2fb7579ed193adeff9f691
shivam <shivampurohit900@gmail.com>
2025-05-10 19:47:14 +0530

    feature:docker compose
    Signed-off-by: shivam <shivampurohit900@gmail.com>

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy import Boolean, Column, Date, DateTime, Float, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship
-from .database import Base
+from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
